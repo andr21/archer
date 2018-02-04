5082f35461cc481b8734086968de7bc047ebb98d
Andrew <andrewwalker_539@hotmail.com>
2018-02-04 09:46:51 +0000

    target practice

--- main.js
+++ main.js
@@ -19,12 +19,24 @@ var arrow = document.getElementById('arrow');
 arrow.width = 60;
 arrow.height = 8;
 
+var target = document.getElementById('target');
+target.width = 30;
+target.height = 30;
 
-function drawDot(x,y,angle){
-    drawRotated(angle, x, y);
+
+function TargetObj(x,y){
+
+ this.x = x;
+ this.y = y;
+ 
+ this.draw = function(){
+  ctx.drawImage(target, this.x, this.y, target.width,target.height);
+ 
+ }
+
 }
 
-function Particle(x,y,m,fb){
+function ArrowObj(x,y,m,fb){
 
 	this.x = x;
 	this.y = y;
@@ -38,10 +50,6 @@ function Particle(x,y,m,fb){
 	 this.fb = -1
 	};
 	
-	//console.log(this.fb.toString());
-
-	//this.vel = {x: 3, y: -3};
-	//this.vel = {x: Math.floor(Math.random() * 3), y: Math.floor(Math.random() * -3)};
 	
 	this.vel = {
 	x: 
@@ -51,75 +59,54 @@ function Particle(x,y,m,fb){
 	 -m*this.fb*Math.sqrt(Math.pow(this.r,2)/(Math.pow(this.m,2) + 1))
 	 //-Math.sqrt(this.r^2*this.m^2/(this.m^2 + 1))
 	};
-	//console.log("x: " + this.vel.x.toString());
-	
+
 	
 	this.acc = {x: 0, y: 0.02};
+	
 	this.ang = findAngle(this.vel.x,this.vel.y);
-
-//console.log(this.ang.toString());
 
 	this.update = function(){
 
-		//if(this.y >= 400 || this.y <= 0){
-		//	this.vel.y = -this.vel.y;
-		//}
-		//if(this.x >= 400 || this.x <= 0){
-		//	this.vel.x = -this.vel.x;
-		//}
-
 		this.x += this.vel.x;
 		this.y += this.vel.y;
-
 
 		this.vel.x += this.acc.x;
 		this.vel.y += this.acc.y;
 		
 		this.ang = findAngle(this.vel.x,this.vel.y);
 		
-		//console.log(this.ang);
 	}
 
 	this.draw = function(){
-		drawDot(this.x,this.y,this.ang)
+		drawRotated(this.ang, this.x, this.y);
 	}
 
 }
 
 function findAngle(xvel, yvel){
-//console.log(Math.atan(yvel/xvel).toString());
-return Math.atan(yvel/xvel);
+  return Math.atan(yvel/xvel);
 }
 
 
-var balls = [];
-
-//balls[0] = new Particle(Math.floor(Math.random() * 400),400);
+var arrows = [];
+var targets = [];
 
 
 function draw(){
 	canvas.width = canvas.width;
 
-	for(var i = 0; i <= balls.length-1; i++) {
-	balls[i].draw();
-	balls[i].update();
+	for(var i = 0; i <= arrows.length-1; i++) {
+	arrows[i].draw();
+	arrows[i].update();
+	}
+	for(var i = 0; i <= targets.length-1; i++) {
+	targets[i].draw();
 	}
 	
-	if(Math.random()>0.98)
-	{
-	//balls[balls.length] = new Particle(Math.floor(Math.random() * 400),400);
-	}
-	
-	pp.innerHTML = balls.length;
+	pp.innerHTML = arrows.length;
 }
 
 setInterval(draw,5);
-
-//canvas.onclick = function(){
-
-	//balls[balls.length] = new Particle(Math.floor(100),400);
-//};
-
 
 
 function drawRotated(radians, centerX, centerY){
@@ -141,65 +128,29 @@ function drawRotated(radians, centerX, centerY){
 canvas.addEventListener('touchstart', handleTouchStart, false);        
 canvas.addEventListener('touchmove', handleTouchMove, false);
 canvas.addEventListener('touchend', handleTouchEnd, false);       
-
-
-//var xDown = null;                                                        
-//var yDown = null;                                                        
+                                                 
 
 var firstTouch = null;
 var lastTouch = null;
 
 
-
-
 function handleTouchStart(evt) {                                         
-    //xDown = evt.touches[0].clientX;                                      
-    //yDown = evt.touches[0].clientY;
     
     firstTouch = evt;
     lastTouch = evt;
+    
+    targets[targets.length] = new TargetObj(firstTouch.touches[0].clientX,firstTouch.touches[0].clientY);
      
-    
-    
-   evt.preventDefault()
-    //console.log("touchStart");                                  
+    evt.preventDefault()
+                                      
 };                                                
 
 function handleTouchMove(evt) {
-    //if ( ! xDown || ! yDown ) {
-    //    return;
-    // }
-
-    //var xUp = evt.touches[0].clientX;                                    
-    //var yUp = evt.touches[0].clientY;
-
-    //var xDiff = xDown - xUp;
-    //var yDiff = yDown - yUp;
-
-    //if ( Math.abs( xDiff ) > //Math.abs( yDiff ) ) {/*most significant*/
-        //if ( xDiff > 0 ) {
-         //j = j + 1;
-     //   } else {
-         //j = j - 1;
-     //   }                       
-   // } else {
-     //   if ( yDiff > 0 ) {
-         //i = i + 1;
-      //  } else { 
-         //i = i - 1;
-     //   }                                                                 
-  //  }
     
     lastTouch = evt;
- 
-    //console.log((-yDiff/xDiff).toString());
-    
-    
-    //console.log((-yDiff/xDiff).toString());
+
  evt.preventDefault()
-    /* reset values */
-   // xDown = null;
-    //yDown = null;                                             
+                                            
 };
 
 
@@ -216,8 +167,10 @@ function handleTouchEnd(evt) {
     
    evt.preventDefault()
    
-   console.log((-yDiff/xDiff).toString());
    
-   balls[balls.length] = new Particle(100,350,-yDiff/xDiff, -xDiff);
+   arrows[arrows.length] = new ArrowObj(100,350,-yDiff/xDiff, -xDiff);
+   
+   firstTouch = null;
+   lastTouch = null;
                                  
 };           
